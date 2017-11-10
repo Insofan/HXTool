cebc8a21b9585fd92edd7924ed19d4c79b311114
Insofan <282132315@qq.com>
2017-11-10 11:26:37 +0800

    [add](dictionary)

--- Example/HXTool/HXViewController.m
+++ Example/HXTool/HXViewController.m
@@ -18,7 +18,7 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    self.view.backgroundColor = [UIColor colorWithRGBString:@"4EBDFB"];
+    self.view.backgroundColor = [UIColor hx_colorWithRGBString:@"4EBDFB"];
     //self.view.backgroundColor = [UIColor colorWithRGBNumber:78 green:189 blue:151];
     UIButton *button = [UIButton hx_buttonWithTitle:@"333" fontSize:12 normalColor:[UIColor blueColor] selectedColor:[UIColor whiteColor]];
     [self.view addSubview:button];
@@ -26,7 +26,7 @@
     UIView *view = [UIView new];
     //view.backgroundColor = [UIColor colorWithRGBString:@"ffffff"];
     //是用随机颜色
-    view.backgroundColor = [UIColor randomColor];
+    view.backgroundColor = [UIColor hx_randomColor];
     view.frame = CGRectMake(100, 300, [UIScreen hx_screenWidth]/2, [UIScreen hx_screenHeight]/6);
     NSLog(@"App version: %@", AppVersion);
     self.array = @[@6, @5, @3];
